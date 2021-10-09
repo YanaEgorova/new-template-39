2bfbda39ca0f8e2bee1a013d9a1e6c34b06888a1
Yana <yanaegorova69.3@gmail.com>
2021-10-09 23:10:16 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,165 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Hot Skillet Handle',
+        price: 6.96,
+        image: './img/img1.png',
+        description: [
+            {
+                text: 'Insulating kitchen accessory protects hands from hot pan handles.',
+            },
+            {
+                text: 'High-quality, heat-resistant silicone creates insulating layer of protection; oven safe up to 475 Degrees F/250 Degrees C.',
+            },
+            {
+                text: 'Works with a variety of cookware handles ranging from 10.5-13.25 inches long, including most cast iron skillets.',
+            },
+            {
+                text: 'DO NOT use holder when coming in contact with open flames, leave the holder on the handle while cooking in the oven, or use with temperatures higher than 250 degrees C/475 degrees F for longer than 1 hour.',
+            },
+            {
+                text: 'DO use as a guard to protect your hands from burns, use the anti-slip grip to slide the holder onto a hot handle, and use to transfer hot items to and from the oven.',
+            },
+            {
+                text: 'Slides on and off easily and stays securely in place while in use; measures 5.6 inches long x 1.9 inches wide; fits skillets with handles 10.5 inches or larger.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Storage Basket',
+        price: 29.99,
+        image: './img/img2.png',
+        description: [
+            {
+                title: 'Basket storage',
+                text: 'Sometimes the best solutions are the small and simple additions. Stylishly declutter and organize your home instantly with the addition of a small storage basket. All while adding a decorative touch to any room that you choose to use for.',
+            },
+            {
+                title: 'Multi-functional',
+                text: 'Designed to be a highly versatile basket to help you keep your home clean and organized. Capable of storing typical household items such as blankets, clothes, magazines and dog toys. From using as a nursery basket to a laundry basket, it is completely up to you.',
+            },
+            {
+                title: 'Two-tone pattern',
+                text: 'Not only will it serve as a purposeful storage solution throughout the home, but it will look good as a standout décor in doing so. Available in 3 colors to match most room themes: blue, beige, and gray – each with ivory as the second tone.',
+            },
+            {
+                title: 'Sturdy handles',
+                text: 'Two arching handles will help assist with transporting or carrying around the house while holding larger or heavy duty items. The collapsible design makes it easier to store items of different shapes and sizes, adding to the multi-functionality.',
+            },
+            {
+                title: 'Dimensions',
+                text: '11.81” x 9.84” x 9.06”',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Hand Truck',
+        price: 64.90,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Glass Filled Nylon Plastic Frame is 30% lighter than steel and designed for maximum strength.',
+            },
+            {
+                text: 'Plastic Frame is factory assembled and is chemical, corrosion and rust resistant for longer life.',
+            },
+            {
+                text: 'Converts in seconds from 2 wheel dolly to 4 wheel cart - 300 lb dolly and 400 lb cart capacity (Product images illustrates both uses, but only one cart is being sold).',
+            },
+            {
+                text: '8" wheels with solid rubber tread and 3" swivel casters.',
+            },
+            {
+                text: 'Three position telescoping handle, built-in frame glides for easy moving up or down stairs and curbs.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png'],
+    },
+    {
+        id: 'product-4',
+        name: 'Electric Kettle',
+        price: 98.90,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'Cool Touch',
+                text: 'This electric hot water kettle features double-walled body keeps water hot longer while the outside remains cool to the touch.',
+            },
+            {
+                title: 'Protective Lid',
+                text: 'The electric kettle features 70° lid opening prevents direct contact with hot steam.',
+            },
+            {
+                title: 'Easy Clean Up',
+                text: 'Seamless stainless-steel kettle for easy, residue-free clean up.',
+            },
+            {
+                title: 'Easy Storing',
+                text: 'Integrated cable winder for convenient storage, Voltage: 120 Volt, Substance: plastic.',
+            },
+            {
+                title: 'Sleek Design',
+                text: 'This electric tea kettle model comes in black.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Airbrush',
+        price: 119.98,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'This is a dual action gravity feed airbrush; comes with instruction manual.',
+            },
+            {
+                text: 'Innovative and efficient precision design is enjoyed by crafters, artists, and hobbyists.',
+            },
+            {
+                text: 'Single (.5) needle/nozzle for spraying all Mediums, inks, dyes, watercolors, acrylics, enamels, lacquers, glazes, latex and etc.',
+            },
+            {
+                text: 'Easy maintenance self- centering nozzle design; acute tapered color well for faster cleaning; Maximum angle trigger clearance and stroke; Patented easy access needle removal system.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.png', './img/img5-3.png'],
+    },
+    {
+        id: 'product-6',
+        name: 'Step Trash Can',
+        price: 148.00,
+        image: './img/img6.png',
+        description: [
+            {
+                text: 'Dual compartments easily separate your recyclables from your household trash.',
+            },
+            {
+                text: 'Fingerprint resistant stainless-steel finish.',
+            },
+            {
+                text: 'Durable cantilever foot pedal mechanism.',
+            },
+            {
+                text: 'Soft close damper lid.',
+            },
+            {
+                text: 'Stay-open lid position option.',
+            },
+            {
+                text: 'Built in deodorizer compartment.',
+            },
+            {
+                text: 'Plastic bag fixer holds trash bags in place.',
+            },
+            {
+                text: 'Both inner liners removable for ease of emptying.',
+            },
+        ],
+        images: ['./img/img6-1.png','./img/img6-2.png','./img/img6-3.png','./img/img6-4.png','./img/img6-5.png'],
+    },
+]
